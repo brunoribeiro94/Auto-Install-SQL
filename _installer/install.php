d12612303a924b27e6e2178779039089c022396a
Bruno Ribeiro <offboard@hotmail.com>
2014-09-14 21:09:42 -0300

    Update install.php

--- _installer/install.php
+++ _installer/install.php
@@ -1,11 +1,12 @@
 <?php
+
 // include file config
 require '../config/config.inc.php';
-
+$sql_execute = 'Run.sql';
 $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 // Check if any error occurred
 if (!mysqli_connect_errno()) {
-    $query = file_get_contents("Run.sql");
+    $query = file_get_contents($sql_execute);
     /* execute multi query */
     if (mysqli_multi_query($mysqli, $query)) {
         header('location: ../index.php');

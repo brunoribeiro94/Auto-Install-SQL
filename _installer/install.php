e96ab8d219c9248486c73a0192e0792078ed8008
Bruno Ribeiro <offboard@hotmail.com>
2014-09-16 20:29:02 -0300

    Update install.php

--- _installer/install.php
+++ _installer/install.php
@@ -5,14 +5,13 @@ require '../config/config.inc.php';
 $sql_execute = 'Run.sql';
 $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 // Check if any error occurred
-if (!mysqli_connect_errno()) {
-    $query = file_get_contents($sql_execute);
-    /* execute multi query */
-    if (mysqli_multi_query($mysqli, $query)) {
+if (mysqli_connect_errno()) {
+ die('An error occurred while connecting to the database');
+} 
+$query = file_get_contents($sql_execute);
+/* execute multi query */
+if (mysqli_multi_query($mysqli, $query)) {
         header('location: ../index.php');
-    } else {
-        header('location: setup-config.php');
-    }
 } else {
-    die('An error occurred while connecting to the database');
+        header('location: setup-config.php');
 }

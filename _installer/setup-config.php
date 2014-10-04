8abcbb8343ae5495ddf2fbe3bf48722a458923a6
Bruno Ribeiro <offboard@hotmail.com>
2014-10-04 15:56:33 -0300

    Update setup-config.php
    GNU style formatter

--- _installer/setup-config.php
+++ _installer/setup-config.php
@@ -10,66 +10,68 @@ require_once('setup-config-html.php');
  * @access public
  * @package setup_config_html
  * */
-class SetupConfig extends SetupConfigHTML {
-
+class SetupConfig extends SetupConfigHTML
+  {
+    
     /**
      * ASCI table for replace
      * @var array 
      */
-    private $ASCITableReplace = array(
-        '&#36;' => '$',
-        '&#60;' => '<',
-        '&#231;' => 'ç',
-        '&#227;' => 'ã',
-        '&#64;' => '@',
-        '&#234;' => 'ê',
-        '&#245;' => 'õ'
-    );
-
+    private $ASCITableReplace = array('&#36;' => '$', '&#60;' => '<', '&#231;' => 'ç', '&#227;' => 'ã', '&#64;' => '@', '&#234;' => 'ê', '&#245;' => 'õ');
+    
     /**
      * Replace special code for create a new file config with format UFT-8
      * @return string
      */
-    private function ReplaceASCIIFwrite($string) {
-        foreach ($this->ASCITableReplace as $k => $v) {
+    private function ReplaceASCIIFwrite($string)
+      {
+        foreach ($this->ASCITableReplace as $k => $v)
+          {
             $string = str_replace($k, $v, $string);
-        }
+          }
         return $string;
-    }
-
+      }
+    
     /**
      * Write a file in UTF-8 format
      * @param String $filename Full Path
      * @param String $content Content that will be written
      */
-    private function WriteUTF8File($filename, $content) {
+    private function WriteUTF8File($filename, $content)
+      {
         $f = fopen($filename, "w");
-        if (!$f) {
+        if (!$f)
+          {
             die('Error was not possible to write the file, check the permissions and the folder was created.');
-        } else {
+          }
+        else
+          {
             fwrite($f, pack("CCC", 0xef, 0xbb, 0xbf)); // Now UTF-8 - Add byte order mark
             fwrite($f, $content);
             fclose($f);
-        }
-    }
-
+          }
+      }
+    
     /**
      * Check conecction
      * @return boolean
      */
-    private function CheckConnection($POST) {
+    private function CheckConnection($POST)
+      {
         @$mysqli = new mysqli($POST['dbhost'], $POST['uname'], $POST['pwd'], $POST['dbname']);
         // Check if any error occurred
         return !mysqli_connect_errno() ? true : false;
-    }
-
+      }
+    
     /**
      * Check database connection and make a file of config
      * @return Void
      */
-    private function MakeFile() {
+    private function MakeFile()
+      {
         $file = '../' . $this->file;
-        if ($this->CheckConnection($_POST) == true) {
+        if ($this->CheckConnection($_POST) == true)
+          {
             $this->WriteUTF8File($file, $this->ReplaceASCIIFwrite($this->FileGenerate($_POST)));
             // defines the permission to 666
             chmod($file, 0666);
@@ -78,30 +80,34 @@ class SetupConfig extends SetupConfigHTML {
                 print $this->HTMLSucessCreatedFileConfig();
             else
                 print $this->HTMLErroUnknow();
-        } else {
+          }
+        else
+          {
             print $this->HTMLErroConnection();
-        }
-    }
-
+          }
+      }
+    
     /**
      * Method Magic
      * @return void
      */
-    public function __construct() {
+    public function __construct()
+      {
         $step = filter_input(INPUT_GET, 'step');
-        switch ($step) {
+        switch ($step)
+        {
             case 1:
                 print $this->HTMLStep2();
                 break;
             case 2:
                 $this->MakeFile();
                 break;
-            default :
+            default:
                 print $this->HTMLStep1();
                 break;
         }
-    }
-
-}
+      }
+    
+  }
 
 new SetupConfig();

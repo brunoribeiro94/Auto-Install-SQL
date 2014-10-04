54da0f5e5a72a2118448d6a23bf48e64dbaa6a6f
Bruno Ribeiro <offboard@hotmail.com>
2014-10-04 15:57:42 -0300

    Update install.php
    GNU style formatter

--- _installer/install.php
+++ _installer/install.php
@@ -2,16 +2,26 @@
 
 // include file config
 require '../config/config.inc.php';
+
+// sql file to execute
 $sql_execute = 'Run.sql';
-$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+// create a new database connection
+$mysqli      = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
 // Check if any error occurred
-if (mysqli_connect_errno()) {
- die('An error occurred while connecting to the database');
-} 
+if (mysqli_connect_errno())
+  {
+    die('An error occurred while connecting to the database');
+  }
 $query = file_get_contents($sql_execute);
+
 /* execute multi query */
-if (mysqli_multi_query($mysqli, $query)) {
-        header('location: ../index.php');
-} else {
-        header('location: setup-config.php');
-}
+if (mysqli_multi_query($mysqli, $query))
+  {
+    header('location: ../index.php');
+  }
+else
+  {
+    header('location: setup-config.php');
+  }

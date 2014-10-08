b8e6d4708ed211adacd29f84044193b998f0a231
Bruno Ribeiro <offboard@hotmail.com>
2014-10-08 12:58:45 -0300

    Create pt-br.php
    Português Brasil

---
+++ _installer/Language/pt-br.php
@@ -0,0 +1,16 @@
+<?php
+
+// Português Brasil
+
+
+define('LOGO', '<a style="background-image:url("%s");" href="%s" tabindex="-1">%s</a>');
+
+define('STEP1_TITLETAG', '%s &rsaquo; Arquivo de Configuração da Instalação');
+define('STEP1_TITLE', 'Bem-vindo ao %s. Antes de começar, precisamos de algumas informações sobre o banco de dados. Você precisa conhecer o valor dos seguintes items antes de continuar.');
+define('STEP1_LIST1', 'Nome do Banco de Dados');
+define('STEP1_LIST2', 'Nome de Usuário do Banco de Dados');
+define('STEP1_LIST3', 'Senha do Banco de Dados');
+define('STEP1_LIST4', 'Sevidor do Banco de Dados');
+define('STEP1_TEXT1', 'Nós vamos usar essas informações para criar um arquivo <code>%s</code>.	<strong>Se por acaso, esta criação automática de arquivo não funcionar, não se preocupe. Tudo o que ele faz é preencher as informações do banco de dados em um arquivo de configuração. Você também pode simplesmente abrir <code>%s</code> num editor de texto, preencher suas informações e salvá-lo como <code>%s</code>.');
+define('STEP1_TEXT2', 'Provavelmente esses itens foram fornecidos pelo seu servidor de hospedagem. Se não tiver essa informação, então você precisa entrar em contato com eles antes de continuar. Se estiver pronto...');
+define('STEP1_BUTTOM', 'Vamos lá!');

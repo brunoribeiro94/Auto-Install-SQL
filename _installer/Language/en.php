62a480c4b89ccf5931d62fc9e5b1178680578632
Bruno Ribeiro <offboard@hotmail.com>
2014-10-09 18:23:06 -0300

    Update en.php
    steps language

--- _installer/Language/en.php
+++ _installer/Language/en.php
@@ -4,3 +4,27 @@
 define('LOGO', <<<EOF
 <a style="background-image:url('%s');" href="%s" tabindex="-1">%s</a>
 EOF
+
+/*
+* Steps
+*/
+define('STEP1_TITLETAG', '%s &rsaquo; Configuration File Installation');
+define('STEP1_TITLE', 'Welcome to %s. Before getting started, we need some information on the database. You will need to know the following items before proceeding.');
+define('STEP1_LIST1', 'Database name');
+define('STEP1_LIST2', 'Database username');
+define('STEP1_LIST3', 'Database password');
+define('STEP1_LIST4', 'Database host');
+define('STEP1_TEXT1', 'We’re going to use this information to create a <code>%s</code>. <strong>	If for any reason this automatic file creation doesn’t work, don’t worry. All this does is fill in the database information to a configuration file. You may also simply open <code>%s</code> in a text editor, fill in your information, and save it as <code>%s</code>.');
+define('STEP1_TEXT2', 'In all likelihood, these items were supplied to you by your Web Host. If you do not have this information, then you will need to contact them before you can continue. If you’re all ready…');
+define('STEP1_BUTTOM', 'Let’s go!');
+define('STEP2_TITLE', 'Welcome to %s. Before getting started, we need some information on the database. You will need to know the following items before proceeding.');
+define('STEP2_TEXT1', 'Below you should enter your database connection details. If you’re not sure about these, contact your host.');
+define('STEP2_LIST1', 'Database Name');
+define('STEP2_DLIST1', 'The name of the database you want to run %s in.');
+define('STEP2_LIST2', 'User Name');
+define('STEP2_DLIST2', 'Your MySQL username');
+define('STEP2_LIST3', 'Password');
+define('STEP2_DLIST3', '&hellip;and your MySQL password.');
+define('STEP2_LIST4', 'Database Host');
+define('STEP2_DLIST4', 'You should be able to get this info from your web host, if <code>localhost</code> does not work.');
+define('STEP2_BUTTOM', 'Submit');

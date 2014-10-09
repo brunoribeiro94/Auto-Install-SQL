465805a8e63288071401df291ec642c712379102
Bruno Ribeiro <offboard@hotmail.com>
2014-10-09 18:05:32 -0300

    Update setup-config-html.php
    GNU formatter style.

--- _installer/setup-config-html.php
+++ _installer/setup-config-html.php
@@ -3,13 +3,14 @@
 /**
  * HTMLs configuration pages
  * @author Bruno Ribeiro <bruno.espertinho@gmail.com>
- * @version 1
+ * @version 1.1
  * @access public
  * @package Config
  * @todo Improve the customization of the configuration file
  * */
-class SetupConfigHTML {
-
+class SetupConfigHTML
+  {
+    
     /**
      * file config required for start the website
      * Note: at the time of verification and creating the configuration file, is always 
@@ -17,57 +18,53 @@ class SetupConfigHTML {
      * @var string 
      */
     public $file = 'config/config.inc.php';
-
+    
     /**
      * Name your project
      * @var str 
      */
     var $name = 'Example';
-
+    
     /**
      * link your project
      * @var str 
      */
     var $link = 'http://example.com';
-
+    
     /**
      * logo your project
      * @var str 
      */
     var $logo = 'images/w-logo-blue.png';
-
+    
     /**
      * Language of pages
      * @var string 
      */
     var $CurrentLanguage = 'pt-br';
-
+    
     /**
      * Rules for replacement
      * @var array
      */
-    var $Rules = array(
-        "&lt;" => "<",
-        "&gt;" => ">",
-        "&quot;" => '"',
-        "&apos;" => "'",
-        "&amp;" => "&",
-    );
-
+    var $Rules = array("&lt;" => "<", "&gt;" => ">", "&quot;" => '"', "&apos;" => "'", "&amp;" => "&");
+    
     /**
      * Metthod Magic
      * Require language file
      */
-    public function __construct() {
-        switch ($this->CurrentLanguage) {
+    public function __construct()
+      {
+        switch ($this->CurrentLanguage)
+        {
             case 'pt-br':
                 include(__DIR__ . '/Language/pt-br.php');
                 break;
             default:
                 break;
         }
-    }
-
+      }
+    
     /**
      * Here is the main configuration file of your application.
      * Note: replace special characters that makes conflict with the EOFPAGE 
@@ -77,7 +74,8 @@ class SetupConfigHTML {
      * @param array $POST
      * @return HTML
      */
-    protected function FileGenerate($POST) {
+    protected function FileGenerate($POST)
+      {
         $year = date('Y');
         return <<<EOFPAGE
 &#60;?php
@@ -144,152 +142,159 @@ define('DB_NAME', '{$POST['dbname']}');
 define('DB_USER', '{$POST['uname']}');
 define('DB_PASS', '{$POST['pwd']}');
 EOFPAGE;
-    }
-
+      }
+    
     /**
      * Convert Special Char
      * @param string $string
      * @return string
      */
-    private function RevertHTML($string) {
-        foreach ($this->Rules as $k => $v) {
+    private function RevertHTML($string)
+      {
+        foreach ($this->Rules as $k => $v)
+          {
             $string = str_replace($k, $v, $string);
-        }
+          }
         return $string;
-    }
-
+      }
+    
     /**
      * Create an HTML for step 1
      * @access protected
      * @return HTML
      */
-    protected function HTMLStep1() {
+    protected function HTMLStep1()
+      {
         $html = new DOMDocument();
         $html->loadHTMLFile("Pages/step1.xml");
         $html->preserveWhiteSpace = false;
-        $html->formatOutput = true;
-
-        $html->getElementById('logo')->nodeValue = sprintf(LOGO, $this->logo, $this->link, $this->name);
+        $html->formatOutput       = true;
+        
+        $html->getElementById('logo')->nodeValue                 = sprintf(LOGO, $this->logo, $this->link, $this->name);
         $html->getElementsByTagName('title')->item(0)->nodeValue = sprintf(STEP1_TITLETAG, $this->name);
-        $html->getElementById('title')->nodeValue = sprintf(STEP1_TITLE, $this->name);
-        $html->getElementById('LIST1')->nodeValue = STEP1_LIST1;
-        $html->getElementById('LIST2')->nodeValue = STEP1_LIST2;
-        $html->getElementById('LIST3')->nodeValue = STEP1_LIST3;
-        $html->getElementById('LIST4')->nodeValue = STEP1_LIST4;
-        $html->getElementById('txt1')->nodeValue = sprintf(STEP1_TEXT1, $this->file, $this->file, $this->file);
-        $html->getElementById('txt2')->nodeValue = STEP1_TEXT2;
-        $html->getElementById('step_1_buttom')->nodeValue = STEP1_BUTTOM;
-
+        $html->getElementById('title')->nodeValue                = sprintf(STEP1_TITLE, $this->name);
+        $html->getElementById('LIST1')->nodeValue                = STEP1_LIST1;
+        $html->getElementById('LIST2')->nodeValue                = STEP1_LIST2;
+        $html->getElementById('LIST3')->nodeValue                = STEP1_LIST3;
+        $html->getElementById('LIST4')->nodeValue                = STEP1_LIST4;
+        $html->getElementById('txt1')->nodeValue                 = sprintf(STEP1_TEXT1, $this->file, $this->file, $this->file);
+        $html->getElementById('txt2')->nodeValue                 = STEP1_TEXT2;
+        $html->getElementById('step_1_buttom')->nodeValue        = STEP1_BUTTOM;
+        
         return $this->RevertHTML($html->saveHTML());
-    }
-
+      }
+    
     /**
      * Create an HTML for step 2
      * @access protected
      * @return HTML
      */
-    protected function HTMLStep2() {
+    protected function HTMLStep2()
+      {
         $html = new DOMDocument();
         $html->loadHTMLFile("Pages/step2.xml");
         $html->preserveWhiteSpace = false;
-        $html->formatOutput = true;
-
-        $html->getElementById('logo')->nodeValue = sprintf(LOGO, $this->logo, $this->link, $this->name);
+        $html->formatOutput       = true;
+        
+        $html->getElementById('logo')->nodeValue                 = sprintf(LOGO, $this->logo, $this->link, $this->name);
         $html->getElementsByTagName('title')->item(0)->nodeValue = sprintf(STEP1_TITLETAG, $this->name);
-
+        
         $html->getElementById('STEP2_TEXT1')->nodeValue = STEP2_TEXT1;
-
-        $html->getElementById('LIST1')->nodeValue = STEP2_LIST1;
+        
+        $html->getElementById('LIST1')->nodeValue  = STEP2_LIST1;
         $html->getElementById('DLIST1')->nodeValue = sprintf(STEP2_DLIST1, $this->name);
-
-        $html->getElementById('LIST2')->nodeValue = STEP2_LIST2;
+        
+        $html->getElementById('LIST2')->nodeValue  = STEP2_LIST2;
         $html->getElementById('DLIST2')->nodeValue = STEP2_DLIST2;
-
-        $html->getElementById('LIST3')->nodeValue = STEP2_LIST3;
+        
+        $html->getElementById('LIST3')->nodeValue  = STEP2_LIST3;
         $html->getElementById('DLIST3')->nodeValue = STEP2_DLIST3;
-
-        $html->getElementById('LIST3')->nodeValue = STEP2_LIST3;
+        
+        $html->getElementById('LIST3')->nodeValue  = STEP2_LIST3;
         $html->getElementById('DLIST3')->nodeValue = STEP2_DLIST3;
-
-        $html->getElementById('LIST4')->nodeValue = STEP2_LIST4;
+        
+        $html->getElementById('LIST4')->nodeValue  = STEP2_LIST4;
         $html->getElementById('DLIST4')->nodeValue = STEP2_DLIST4;
-
-
+        
+        
         $input = $html->getElementById('send');
         $input->removeAttribute('value');
         $input->setAttribute('value', STEP2_BUTTOM);
-
+        
         return $this->RevertHTML($html->saveHTML());
-    }
-
+      }
+    
     /**
      * Create an HTML when it is not possible to connect to the database
      * @access protected
      * @return type
      */
-    protected function HTMLErroConnection() {
+    protected function HTMLErroConnection()
+      {
         $html = new DOMDocument();
         $html->loadHTMLFile("Pages/ErroConnection.xml");
         $html->preserveWhiteSpace = false;
-        $html->formatOutput = true;
-
+        $html->formatOutput       = true;
+        
         $html->getElementsByTagName('title')->item(0)->nodeValue = sprintf(STEP1_TITLETAG, $this->name);
-
-        $html->getElementById('TEXT1')->nodeValue = ERRO_CONNETION_TEXT1;
-        $html->getElementById('TEXT2')->nodeValue = ERRO_CONNETION_TEXT2;
-        $html->getElementById('TEXT3')->nodeValue = ERRO_CONNETION_TEXT3;
-        $html->getElementById('TEXT4')->nodeValue = ERRO_CONNETION_TEXT4;
-        $html->getElementById('TEXT5')->nodeValue = ERRO_CONNETION_TEXT5;
-        $html->getElementById('TEXT6')->nodeValue = ERRO_CONNETION_TEXT6;
+        
+        $html->getElementById('TEXT1')->nodeValue  = ERRO_CONNETION_TEXT1;
+        $html->getElementById('TEXT2')->nodeValue  = ERRO_CONNETION_TEXT2;
+        $html->getElementById('TEXT3')->nodeValue  = ERRO_CONNETION_TEXT3;
+        $html->getElementById('TEXT4')->nodeValue  = ERRO_CONNETION_TEXT4;
+        $html->getElementById('TEXT5')->nodeValue  = ERRO_CONNETION_TEXT5;
+        $html->getElementById('TEXT6')->nodeValue  = ERRO_CONNETION_TEXT6;
         $html->getElementById('buttom')->nodeValue = ERRO_CONNETION_BUTTOM;
-
+        
         return $this->RevertHTML($html->saveHTML());
-    }
-
+      }
+    
     /**
      * create an HTML when the file was created successfully
      * @access protected
      * @return HTML
      */
-    protected function HTMLSucessCreatedFileConfig() {
+    protected function HTMLSucessCreatedFileConfig()
+      {
         $html = new DOMDocument();
         $html->loadHTMLFile("Pages/SucessCreated.xml");
         $html->preserveWhiteSpace = false;
-        $html->formatOutput = true;
-
-        $html->getElementById('logo')->nodeValue = sprintf(LOGO, $this->logo, $this->link, $this->name);
+        $html->formatOutput       = true;
+        
+        $html->getElementById('logo')->nodeValue                 = sprintf(LOGO, $this->logo, $this->link, $this->name);
         $html->getElementsByTagName('title')->item(0)->nodeValue = sprintf(STEP1_TITLETAG, $this->name);
-
+        
         $html->getElementById('text')->nodeValue = sprintf(SUCCESS_TEXT, $this->name);
-
+        
         $html->getElementById('buttom')->nodeValue = SUCCESS_BUTTOM;
-
+        
         return $this->RevertHTML($html->saveHTML());
-    }
-
+      }
+    
     /**
      * create an HTML when some error occurs
      * @access protected
      * @return HTML
      */
-    protected function HTMLErroUnknow() {
+    protected function HTMLErroUnknow()
+      {
         $html = new DOMDocument();
         $html->loadHTMLFile("Pages/ErroUnknow.xml");
         $html->preserveWhiteSpace = false;
-        $html->formatOutput = true;
-
-        $html->getElementById('logo')->nodeValue = sprintf(LOGO, $this->logo, $this->link, $this->name);
+        $html->formatOutput       = true;
+        
+        $html->getElementById('logo')->nodeValue                 = sprintf(LOGO, $this->logo, $this->link, $this->name);
         $html->getElementsByTagName('title')->item(0)->nodeValue = sprintf(STEP1_TITLETAG, $this->name);
-
+        
         $html->getElementById('ERROUNKNOW_TEXT1')->nodeValue = sprintf(ERROUNKNOW_TEXT1, $this->file);
         $html->getElementById('ERROUNKNOW_TEXT2')->nodeValue = sprintf(ERROUNKNOW_TEXT2, $this->file);
         $html->getElementById('ERROUNKNOW_TEXT3')->nodeValue = ERROUNKNOW_TEXT3;
-
+        
         $html->getElementById('wp-config')->nodeValue = $this->FileGenerate($_POST);
-
+        
         $html->getElementById('buttom')->nodeValue = SUCCESS_BUTTOM;
-
+        
         return $this->RevertHTML($html->saveHTML());
-    }
-
-}
+      }
+    
+  }

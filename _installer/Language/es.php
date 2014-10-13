bba215a26da1b7b9ce677aa4a848b28739bc2dfa
Bruno Ribeiro <offboard@hotmail.com>
2014-10-13 15:49:36 -0300

    Update es.php

--- _installer/Language/es.php
+++ _installer/Language/es.php
@@ -7,4 +7,13 @@ define('LOGO', <<<EOF
 EOF
 );
 
+/**
+* Erros
+*/
+// unknown error
+define('ERROUNKNOW_TEXT1', 'Lo siento, pero no puedo escribir la <code>%s</code>.');
+define('ERROUNKNOW_TEXT2', 'Puede crear el <code>% s </ code> manualmente y pegue el texto siguiente en él.');
+define('ERROUNKNOW_TEXT3', 'Después de que hayas hecho esto, haga clic em "Ejecutar la instalación."');
+
+
 // finish

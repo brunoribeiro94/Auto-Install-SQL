1846e55e5738cfc30c67edcfe93fedcb7b9056e7
Bruno Ribeiro <offboard@hotmail.com>
2014-10-14 17:17:01 -0300

    Update it.php
    unknow erro

--- _installer/Language/it.php
+++ _installer/Language/it.php
@@ -8,5 +8,13 @@ EOF
 );
 
 // sucess
-define('SUCCESS_TEXT', 'Ottimo! Avete completato questa parte dell'installazione. Ora %s può comunicare con loro database. Se sei pronto, è il momento di &hellip;');
-define('SUCCESS_BUTTOM', 'installare');
+define('SUCCESS_TEXT', "Ottimo! Avete completato questa parte dell'installazione. Ora %s può comunicare con loro database. Se sei pronto, è il momento di &hellip;");
+define('SUCCESS_BUTTOM', 'Installare');
+
+/**
+* Erros
+*/
+// unknown error
+define('ERROUNKNOW_TEXT1', 'Impossibile scrivere il file <code>%s</code>.');
+define('ERROUNKNOW_TEXT2', 'È possibile creare il file <code>%s</code> manualmente e incollare il seguente testo su di esso.');
+define('ERROUNKNOW_TEXT3', 'Dopo aver completato click &#8220;Installare&#8221;.');

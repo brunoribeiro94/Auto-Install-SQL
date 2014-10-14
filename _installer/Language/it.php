cc738de508992d19f7e1b39ac78009040946f8fc
Bruno Ribeiro <offboard@hotmail.com>
2014-10-14 17:18:48 -0300

    Update it.php
    translate connection error

--- _installer/Language/it.php
+++ _installer/Language/it.php
@@ -18,3 +18,12 @@ define('SUCCESS_BUTTOM', 'Installare');
 define('ERROUNKNOW_TEXT1', 'Impossibile scrivere il file <code>%s</code>.');
 define('ERROUNKNOW_TEXT2', 'È possibile creare il file <code>%s</code> manualmente e incollare il seguente testo su di esso.');
 define('ERROUNKNOW_TEXT3', 'Dopo aver completato click &#8220;Installare&#8221;.');
+
+// connection error
+define('ERRO_CONNETION_TEXT1', 'Errore durante la connessione al database');
+define('ERRO_CONNETION_TEXT2', 'Potrebbe significare che le informazioni di nome utente e password in vostro <code>%s </code> non sono corrette o che non si riesce a contattare il server di database in <code>localhost</code> . Ciò può significare che il server che ospita il database è giù.');
+define('ERRO_CONNETION_TEXT3', 'Sei sicuro di aver il nome utente e la password corretti?');
+define('ERRO_CONNETION_TEXT4', 'Sei sicuro di aver digitato il nome del server di hosting?');
+define('ERRO_CONNETION_TEXT5', 'Sei sicuro che il server database sia in esecuzione?');
+define('ERRO_CONNETION_TEXT6', 'Se non siete sicuri che cosa significano questi termini probabilmente si dovrebbe contattare il proprio server di hosting.');
+define('ERRO_CONNETION_BUTTOM', 'Riprova');

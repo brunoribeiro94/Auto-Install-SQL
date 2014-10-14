8302218b4dd4533cdf8f1d7ce7ce7ded88782e4d
Bruno Ribeiro <offboard@hotmail.com>
2014-10-14 17:15:06 -0300

    Create it.php
    Italiano incomplete

---
+++ _installer/Language/it.php
@@ -0,0 +1,12 @@
+<?php
+
+// Italiano
+
+define('LOGO', <<<EOF
+<a style="background-image:url('%s');" href="%s" tabindex="-1">%s</a>
+EOF
+);
+
+// sucess
+define('SUCCESS_TEXT', 'Ottimo! Avete completato questa parte dell'installazione. Ora %s può comunicare con loro database. Se sei pronto, è il momento di &hellip;');
+define('SUCCESS_BUTTOM', 'installare');

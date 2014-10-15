b32ec7cfac54dc227b216f2098b264b7d8bc1a06
Bruno Ribeiro <offboard@hotmail.com>
2014-10-15 15:56:07 -0300

    Update it.php
    Finish Italiano language translate

--- _installer/Language/it.php
+++ _installer/Language/it.php
@@ -9,28 +9,28 @@ EOF
 
 
 /*
-* Steps FINISH
+* Steps
 */
-define('STEP1_TITLETAG', '%s &rsaquo; Arquivo de Configuração da Instalação');
-define('STEP1_TITLE', 'Bem-vindo ao %s. Antes de começar, precisamos de algumas informações sobre o banco de dados. Você precisa conhecer o valor dos seguintes items antes de continuar.');
-define('STEP1_LIST1', 'Nome do Banco de Dados');
-define('STEP1_LIST2', 'Nome de Usuário do Banco de Dados');
-define('STEP1_LIST3', 'Senha do Banco de Dados');
-define('STEP1_LIST4', 'Sevidor do Banco de Dados');
-define('STEP1_TEXT1', 'Nós vamos usar essas informações para criar um arquivo <code>%s</code>. <strong>Se por acaso, esta criação automática de arquivo não funcionar, não se preocupe. Tudo o que ele faz é preencher as informações do banco de dados em um arquivo de configuração. Você também pode simplesmente abrir <code>%s</code> num editor de texto, preencher suas informações e salvá-lo como <code>%s</code>.');
-define('STEP1_TEXT2', 'Provavelmente esses itens foram fornecidos pelo seu servidor de hospedagem. Se não tiver essa informação, então você precisa entrar em contato com eles antes de continuar. Se estiver pronto...');
-define('STEP1_BUTTOM', 'Vamos lá!');
-define('STEP2_TITLE', 'Bem-vindo ao %s. Antes de começar, precisamos de algumas informações sobre o banco de dados. Você precisa conhecer o valor dos seguintes items antes de continuar.');
-define('STEP2_TEXT1', 'Abaixo você deve digitar suas informações de conexão com o banco de dados. Se você não tem certeza quais são, contate sua hospedagem.');
-define('STEP2_LIST1', 'Nome do Banco de Dados');
-define('STEP2_DLIST1', 'Nome do banco de dados onde você quer instalar o %s.');
-define('STEP2_LIST2', 'Nome de usuário');
-define('STEP2_DLIST2', 'Usuário de MySQL');
-define('STEP2_LIST3', 'Senha');
-define('STEP2_DLIST3', '&hellip;e sua senha do MySQL.');
-define('STEP2_LIST4', 'Servidor da banco de dados');
-define('STEP2_DLIST4', 'Você deve obter esta informação do seu servidor de hospedagem, se <code>localhost</code> não funcionar.');
-define('STEP2_BUTTOM', 'Enviar');
+define('STEP1_TITLETAG', '%s &rsaquo; Installazione del file di configurazione');
+define('STEP1_TITLE', 'Benvenuti ao %s. Prima di iniziare, abbiamo bisogno di alcune informazioni sul database. È necessario conoscere il valore dei seguenti elementi prima di procedere.');
+define('STEP1_LIST1', 'Nome del database');
+define('STEP1_LIST2', 'Nome utente database');
+define('STEP1_LIST3', 'Database delle password');
+define('STEP1_LIST4', 'Server di database');
+define('STEP1_TEXT1', 'Useremo queste informazioni per creare un file <code>%s</code>. <strong>Se per caso, la creazione automatica dei file non funziona, non ti preoccupare. Tutto quello che fa è inserire le informazioni dal database in un file di configurazione. È anche possibile aprire semplicemente <code>%s</code> in un editor di testo, inserire le informazioni e salvarlo come <code>%s</code>.');
+define('STEP1_TEXT2', 'Probabilmente questi elementi sono stati forniti dal vostro server di hosting. Se non si dispone di queste informazioni, allora avete bisogno di contattarli prima di continuare. Se sei pronto ...');
+define('STEP1_BUTTOM', 'Venire!');
+define('STEP2_TITLE', 'Benvenuti ao %s. Prima di iniziare, abbiamo bisogno di alcune informazioni sul database. È necessario conoscere il valore dei seguenti elementi prima di procedere.');
+define('STEP2_TEXT1', 'Qui di seguito è necessario immettere le informazioni di connessione al database. Se non siete sicuri di quello che sono, contattare il provider di hosting.');
+define('STEP2_LIST1', 'Nome del database');
+define('STEP2_DLIST1', 'Nome del database in cui si desidera installare %s.');
+define('STEP2_LIST2', 'Nome utente');
+define('STEP2_DLIST2', 'MySQL User');
+define('STEP2_LIST3', 'Password');
+define('STEP2_DLIST3', '&hellip, e la tua password MySQL.');
+define('STEP2_LIST4', 'Il server di database');
+define('STEP2_DLIST4', 'Si dovrebbe ottenere queste informazioni dal vostro server di hosting, se <code>localhost</code> non funziona.');
+define('STEP2_BUTTOM', 'Inviare');
 
 // sucess
 define('SUCCESS_TEXT', "Ottimo! Avete completato questa parte dell'installazione. Ora %s può comunicare con loro database. Se sei pronto, è il momento di &hellip;");

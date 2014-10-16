f5d8726c410f7d450c39bb08b1cf75903118274c
Bruno Ribeiro <offboard@hotmail.com>
2014-10-16 20:39:32 -0300

    Create ru.php

---
+++ _installer/Language/ru.php
@@ -0,0 +1,17 @@
+<?php
+// Русский - Beta
+define('LOGO', <<<EOF
+<a style="background-image:url('%s');" href="%s" tabindex="-1">%s</a>
+EOF
+);
+
+// sucess
+define('SUCCESS_TEXT', 'Очень хорошо! Вы завершили эту часть установки. Теперь %s может общаться с их базы данных. Если вы готовы, пора&hellip;');
+define('SUCCESS_BUTTOM', 'Установите');
+/**
+* Erros
+*/
+// unknown error
+define('ERROUNKNOW_TEXT1', 'Не удалось записать файл <code>%s</code>.');
+define('ERROUNKNOW_TEXT2', 'Вы можете создать файл <code>%s</code> вручную и вставьте следующий текст на нем.');
+define('ERROUNKNOW_TEXT3', 'После завершения нажмите кнопку &#8220;Установить&#8221;.');
